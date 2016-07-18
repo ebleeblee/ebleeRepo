596d5dcff9a011afa19b924240c3330fe6b9206d
ebleeblee <eblee@rockplace.co.kr>
2016-07-18 15:00:58 +0900

    hotfix branch change a code

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("New hello world~~~~~~~~~");
+		System.out.println("New Hi world");
 	}
 
 }
