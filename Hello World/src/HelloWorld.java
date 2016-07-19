cfe298c029ebb9dd07061b2e42a53aec0dc29812
ebleeblee <eblee@rockplace.co.kr>
2016-07-19 20:07:51 +0900

    commit123

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("New hello world~~~~~~~~~");
-		System.out.println("New Hi worldvv");
+		System.out.println("New Hi world");
+		System.out.println("hi hello");
 	}
 
 }
